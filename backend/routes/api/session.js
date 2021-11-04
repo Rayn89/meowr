90050b72f7e48912ecd1262e2939cb93861b5c81
Raymond Nehring <rnehring89@gmail.com>
2021-11-04 15:22:31 -0500

    Completed backend

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,19 +6,34 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
-// LOG-IN
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
+// Log in
 router.post(
-  "/",
+  '/',
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.login({ credential, password });
 
     if (!user) {
-      const err = new Error("Login failed");
+      const err = new Error('Login failed');
       err.status = 401;
-      err.title = "Login failed";
-      err.errors = ["The provided credentials were invalid."];
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
       return next(err);
     }
 
@@ -27,14 +42,29 @@ router.post(
     return res.json({
       user,
     });
-  })
+  }),
 );
+
 
 // LOG-OUT
 router.delete("/", (_req, res) => {
   res.clearCookie("token");
   return res.json({ message: "success" });
 });
+
+// Restore session user
+router.get(
+  '/',
+  restoreUser,
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
+);
 
 
 
